4e061c4a8e41f3295b0e7306c1b75ece17d57628
swapnil shindemeshram <smeshram258@gmail.com>
2018-07-20 09:18:59 +0530

    added registeration page and made changes to login

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,16 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
 
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
 app.config.from_object(Config)
-
+Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app=app, db=db)
+
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,32 @@
 from flask_wtf import  FlaskForm
 from wtforms import StringField, BooleanField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from .models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign in')
+
+
+
+class RegisterationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email() ])
+    password = PasswordField('password', validators=[DataRequired()])
+    password2 = PasswordField('password2', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please select a different email') 
+
 
     
